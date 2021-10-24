7c43f08718388e1a43923c9cb46d9e93d83a4470
shree2410 <91643353+shree2410@users.noreply.github.com>
2021-10-24 10:58:18 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Boolean_Variable
+{
+    class Program
+    {
+        
+
+        static void testDevi(string value)
+        {
+            bool a = Convert.ToBoolean(value);
+            if (a)
+            {
+                Console.WriteLine("Yes");
+            } else
+            {
+                Console.WriteLine("No");
+            }
+        }
+
+      
+        public static void Main(string[] args)
+        {
+           Console.WriteLine("Boolean Variable");
+           Program.testDevi("true");
+            Program.testDevi("false");
+           
+            /*
+           if (args.Length<=0)
+                {
+
+                Console.WriteLine("No args passed");
+
+            } else { 
+            bool a = Convert.ToBoolean(args[0]);
+            
+            if (a)
+            {
+                Console.WriteLine("Yes");
+            } else if (!a)
+            {
+                Console.WriteLine("No");
+            }
+            }
+            */
+
+        }
+    }
+}
